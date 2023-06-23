c275ec115ef8514c72c1e81c8760a3b657d18087
Moritz Firsching <firsching@google.com>
2023-06-23 13:12:27 +0200

    add update_script

---
+++ scripts/update_dumps.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -x
+
+# Test case corpus path
+CORPUS=$1
+
+# Results path
+DUMP_PATH=$2
+
+# Run conformance tests on all decoders
+# djxl
+python3 ./third_party/conformance/scripts/conformance.py --decoder "./third_party/libjxl/build/tools/djxl" --corpus $CORPUS --results=$DUMP_PATH/dump_djxl.json
+# djxl via png
+python3 ./third_party/conformance/scripts/conformance.py --decoder "python3 scripts/wrap_png.py --decoder './third_party/libjxl/build/tools/djxl %s %s'" --corpus $CORPUS --results=$DUMP_PATH/dump_djxl_via_png.json
+# jxl-oxide
+python3 ./third_party/conformance/scripts/conformance.py --decoder "python3 scripts/wrap_png.py --decoder 'jxl-dec %s -o %s'" --corpus $CORPUS --results=$DUMP_PATH/dump_jxl-dec.json
+# jxlatte
+python3 ./third_party/conformance/scripts/conformance.py --decoder "python3 scripts/wrap_png.py --decoder 'java -jar ./third_party/jxlatte/build/java/jxlatte.jar %s %s'" --corpus $CORPUS --results=$DUMP_PATH/dump_jxlatte.json

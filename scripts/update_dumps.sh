5290ff6977b804af29fdb787c3ce155d59d676c3
Moritz Firsching <firsching@google.com>
2025-06-05 11:22:44 +0200

    add jxl-rs (#28)
    * add jxl-rs
    
    * Automation fixes.
    
    - Made it simple to update the index.html for embedding the json data.
    - Made the wrap_png.py script find the icc profile files even when called from unexpected directories.
    
    ---------
    
    Co-authored-by: Martin Bruse <zondolfin@gmail.com>

--- scripts/update_dumps.sh
+++ scripts/update_dumps.sh
@@ -25,6 +25,10 @@ DUMP_PATH=$2
 python3 ./third_party/conformance/scripts/conformance.py --decoder "./third_party/libjxl/build/tools/djxl" --corpus $CORPUS --results=$DUMP_PATH/dump_djxl.json --lax
 # djxl via png
 python3 ./third_party/conformance/scripts/conformance.py --decoder "python3 scripts/wrap_png.py --decoder './third_party/libjxl/build/tools/djxl %s %s --bits_per_sample 16'" --corpus $CORPUS --results=$DUMP_PATH/dump_djxl_via_png.json --lax
+# jxl-rs
+python3 ./third_party/conformance/scripts/conformance.py --decoder "python3 scripts/wrap_jxl-rs.py --decoder ' third_party/jxl-rs/target/release/jxl_cli %s %s --icc-out %s'" --corpus $CORPUS --results=$DUMP_PATH/dump_jxl-rs.json --lax
+# jxl-rs via png
+python3 ./third_party/conformance/scripts/conformance.py --decoder "python3 scripts/wrap_png.py --decoder ' third_party/jxl-rs/target/release/jxl_cli %s %s '" --corpus $CORPUS --results=$DUMP_PATH/dump_jxl-rs_via_png.json --lax
 # jxl-oxide
 python3 ./third_party/conformance/scripts/conformance.py --decoder "python3 scripts/wrap_jxl_oxide.py --decoder 'jxl-oxide %s --icc-output %s -o %s -f npy'" --corpus $CORPUS --results=$DUMP_PATH/dump_jxl-oxide.json --lax
 # jxlatte
